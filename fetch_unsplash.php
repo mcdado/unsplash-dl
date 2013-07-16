82d1e395cadb54de72bae7699b7f1274258c1704
David Gasperoni <mcdado@gmail.com>
2013-07-16 23:49:48 +0200

    Functioning fetching and pushing into arrays

--- fetch_unsplash.php
+++ fetch_unsplash.php
@@ -1,25 +1,26 @@
 <?php
 
+if ()
 $r = new HttpRequest('http://unsplash.com/api/read', HttpRequest::METH_GET);
-//$r->setOptions(array('lastmodified' => filemtime('local.rss')));
+$r->setOptions(array('lastmodified' => filemtime('local.rss')));
 $r->addQueryData(array('type' => 'photo', 'num' => 50));
 try {
     $r->send();
     if ($r->getResponseCode() == 200) {
         // file_put_contents('local.rss', $r->getResponseBody());
-		//$feed = simplexml_load_string($r->getResponseBody());
-		$feed = new SimpleXMLElement($r->getResponseBody());
-		//var_dump($feed);
+		$feed = simplexml_load_string($r->getResponseBody());
+		//$feed = new SimpleXMLElement($r->getResponseBody());
 		
 		$links = array();
-		foreach ( $feed->posts as $post ) {
-			$target = 'photo-link-url';
-			$links[] = $post->$target;
+		foreach ( $feed->posts->post as $entry ) {
+			$links[] = (string)$entry->{'photo-link-url'};
 		}
-		print_r($links);
     }
 } catch (HttpException $ex) {
     echo $ex;
 }
+
+print_r($links);
+
 
 ?>
