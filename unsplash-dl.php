e184e038cdfd1102a592c700c17f08ea6a999e0a
David Gasperoni <web@3dprint.it>
2014-06-12 18:13:40 +0200

    Major rewrite to make compatible with pecl_http2.

--- unsplash-dl.php
+++ unsplash-dl.php
@@ -6,19 +6,30 @@ class UnsplashFetch {
     
     private $location,
             $feed,
+            $logging,
             $log_file,
-            $log_handle;
+            $log_handle,
+            $client,
+            $downloads;
                 
-    function __construct($url, $path, $log) {
+    function __construct($url, $picture_path, $log = false, $log_path = "") {
         $this->feed = $url;
-        $this->location = $path;
-        $this->log_file = $log;
-        
-        if (!file_exists($path)) {
-            mkdir($path, 0777, true);
+        $this->logging = $log;
+        $this->location = $picture_path;
+        if ( $log_path == "") {
+            $this->log_file = "";
+        } else {
+            $this->log_file = $log_path;
+            $this->log_handle = fopen($this->log_file, 'a') or die('Cannot open log file.');
         }
         
-        $this->log_handle = fopen($this->log_file, 'a') or die('Cannot open log file:  ' . $this->log_file);
+        $this->client = new http\Client;
+        
+        if (!file_exists($picture_path)) {
+            mkdir($picture_path, 0777, true);
+        }
+        
+        
     }
     
     function terminate() {
@@ -34,93 +45,116 @@ class UnsplashFetch {
         
         $this->sendLog("Unsplash-dl started.");
         
-        $r = new HttpRequest($this->feed, HttpRequest::METH_GET);
-        
-        if ( file_exists( $this->location . '/unsplash.rss' ) )
-            $r->setOptions(array('lastmodified' => filemtime($this->location . '/unsplash.rss')));
-        
-        $r->addHeaders(array('User-Agent' => 'unsplash-dl'));
-        $r->addQueryData(array('type' => 'photo'));
-        $r->addQueryData(array('num' => '30'));
-        
+        $request = new http\Client\Request("GET", $this->feed, array("User-Agent" => "unsplash-dl (https://github.com/mcdado/unsplash-dl)"));
+        $request->addQuery(new http\QueryString("type=photo"));
+
+        if ( file_exists($this->location . '/unsplash.rss') ) {
+            $request->setHeader('If-Modified-Since', filemtime($this->location . '/unsplash.rss'));
+        }
+
         try {
-            $r->send();
+            $this->client->enqueue($request)->send();
+            $response = $this->client->getResponse($request);
             
-            if ($r->getResponseCode() == 200) {
-                file_put_contents($this->location . '/unsplash.rss', $r->getResponseBody() );
-                $body = simplexml_load_string($r->getResponseBody());
+            if ($response->getResponseCode() == 200) {
+                $body = $response->getBody();
+                file_put_contents($this->location . '/unsplash.rss', $body);
+                $parsed_body = simplexml_load_string($body);
+                unset($body);
                 
-                foreach ( $body->posts->post as $entry ) {
+                foreach ( $parsed_body->posts->post as $entry ) {
+                    $extracted_link = "";
                     $extracted_link = (string)$entry->{'photo-link-url'};
-                    $redirection = $this->getRedirectUrl($extracted_link);
-                    $clean_link = $redirection ? $redirection : $extracted_link;
-                    $this->fetchLink($clean_link);
+                    if ( $extracted_link != "" ) {
+                        $redirection = $this->getRedirectUrl($extracted_link);
+                        $clean_link = $redirection ? $redirection : $extracted_link;
+                        $this->downloads[] = $clean_link;
+                    }
                 }
+                unset($parsed_body);
             } else {
-                $this->sendLog("Response Code: " . $r->getResponseCode() );
+                $this->sendLog("Feed Response Code: " . $response->getResponseCode() );
             }
-        } catch (HttpException $ex) {
-            $this->sendLog("Raised Exception: " . $ex);
+        } catch (http\Exception $ex) {
+            $this->sendLog("Feed Raised Exception: " . $ex);
         }
         
+        $this->sendLog("Beginning to fetch links.");
+        $this->fetchLinks();
     }
-    
-    private function sendLog($message){
-        fwrite($this->log_handle, "[" . date('Y-m-d H:i:s') . "]" . " " . $message . "\n");
+
+    private function sendLog($message) {
+        if ( $this->logging === true ) {
+            if ( $this->log_file == "" ) {
+                echo $message . "\n";
+            } else {
+                fwrite($this->log_handle, "[" . date('Y-m-d H:i:s') . "]" . " " . $message . "\n");
+            }
+        }
     }
     
     private function getRedirectUrl($url) {
+        $this->sendLog("Getting redirected for " . $url);
         stream_context_set_default(array(
             'http' => array(
                 'method' => 'HEAD'
             )
         ));
-        $headers = get_headers($url, 1);
-        if ($headers !== false && isset($headers['Location'])) {
-            return $headers['Location'];
+        try {
+            $headers = get_headers($url, 1);
+            if ($headers !== false && isset($headers['Location'])) {
+                $this->sendLog("`-> Redirected to " . $headers['Location']);
+                return $headers['Location'];
+            }
+        } catch (Exception $ex) {
+            $this->sendLog("Couldn't get redirected URL.");
         }
+        $this->sendLog("`-> No redirection.");
         return false;
     }
-    
-    private function fetchLink($link) {
+
+    private function fetchLinks() {
         
-        $parsed_link = parse_url($link);
-        if ( $parsed_link == false )
-            return false;
-        
-        $file_name = basename($parsed_link['path']);
-        $final_link = str_replace(" ", "%20", $link);
-        
-        $this->sendLog("Detected link: " . $final_link);
-        
-        if ( ! file_exists ($this->location . "/" . $file_name) ) {
-            
-            $r = new HttpRequest($final_link, HttpRequest::METH_GET);
-            $r->addHeaders(array('User-Agent' => 'unsplash-dl'));
-            
+        foreach ($this->downloads as $k => $link ) {
+            // Normalizing the URL early
+            $this->downloads[$k] = str_replace(" ", "%20", $link);
+
+            $parsed_link = parse_url($link);
+            if ( $parsed_link == false ) {
+                unset($this->downloads[$k]);
+                break;
+            }
+
+            $file_name = basename($parsed_link['path']);
+            if ( file_exists($this->location . "/" . $file_name) ) {
+                unset($this->downloads[$k]);
+            }
+        }
+
+        foreach ($this->downloads as $d_link) {
+            $d_request = new http\Client\Request("GET", $d_link);
             try {
-                $r->send();
-                if ($r->getResponseCode() == 200) {
-                    file_put_contents($this->location . "/" . $file_name, $r->getResponseBody());
-                    $this->sendLog("Succesfully downloaded " . $file_name);
-                    
+                $this->client->enqueue($d_request)->send();
+                $d_name = basename($d_request->getRequestUrl());
+                $d_response = $this->client->getResponse();
+                if ($d_response->getResponseCode() == 200) {
+                    file_put_contents($this->location . "/" . $d_name, $d_response->getBody());
+                    $this->sendLog("Succesfully downloaded " . $d_name );
+
                 } else {
-                    $this->sendLog( " " . $file_name . " reported a status code: " . $r->getResponseCode() );
-                     
+                    $this->sendLog( $file_name . " reported a status code: " . $d_response->getResponseCode() );
+
                 }
-            } catch (HttpException $ex) {
+            } catch (http\Exception $ex) {
                 $this->sendLog("Raised Exception: " . $ex);
             }
-            
-        } else {
-            $this->sendLog($file_name . " already downloaded. Skipping.");
         }
     }
 }
 
 $unsplash = new UnsplashFetch(  'http://unsplash.com/api/read',
-                                '/Users/' . get_current_user() . '/Pictures/Unsplash',
-                                '/Users/' . get_current_user() . '/Library/Logs/com.mcdado.unsplash.log');
+                                getenv("HOME") . '/Pictures/Unsplash',
+                                true);
 $unsplash->init();
 $unsplash->terminate();
 
